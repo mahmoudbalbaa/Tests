e8dec1616039933e40e8fde8bb200e20bbbc0416
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-10-15 14:42:31 -0700

    print_num funcs

--- _errorprint.c
+++ _errorprint.c
@@ -1,19 +1,13 @@
 #include "main.h"
 
 /**
- * print_prompt - Display Shell Prompt
- */
-void print_prompt(void)
-{
-	PRINTER("$ ");
-}
-/**
  * _errorprint - Display Error Based on Command and How Many Time Shell Looped
  * @input:User Input
  * @counter:Simple Shell Count Loop
  * @argv:Program Name
  * Return: Void
  */
+
 void _errorprint(char *input, int counter, char **argv)
 {
 	char *er;
