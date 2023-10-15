cccf1ddc33e5a6467a04a26cfafef76dde2f29b6
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-10-15 13:00:27 -0700

    more funcs update

--- _cd.c
+++ _cd.c
@@ -1,46 +1,12 @@
 #include "main.h"
-/**
- * _bul_exit - Exit Statue Shell
- * @cmd: Parsed Command
- * @input: User Input
- * @argv:Program Name
- * @c:Excute Count
- * Return: Void (Exit Statue)
- */
-void  _bul_exit(char **cmd, char *input, char **argv, int c)
-{
-	int statue, i = 0;
-
-	if (cmd[1] == NULL)
-	{
-		free(input);
-		free(cmd);
-		exit(EXIT_SUCCESS);
-	}
-	while (cmd[1][i])
-	{
-		if (_isalpha_(cmd[1][i++]) != 0)
-		{
-			_perror_(argv, c, cmd);
-			break;
-		}
-		else
-		{
-			statue = _atoi_(cmd[1]);
-			free(input);
-			free(cmd);
-			exit(statue);
-		}
-	}
-}
-
 
 /**
- * _cd - Change Dirctorie
+ * _cd - Change Dirctory
  * @cmd: Parsed Command
  * @er: Statue Last Command Excuted
  * Return: 0 Succes 1 Failed (For Old Pwd Always 0 Case No Old PWD)
  */
+
 int _cd(char **cmd, __attribute__((unused))int er)
 {
 	int value = -1;
@@ -49,9 +15,7 @@ int _cd(char **cmd, __attribute__((unused))int er)
 	if (cmd[1] == NULL)
 		value = chdir(getenv("HOME"));
 	else if (_strcmp_(cmd[1], "-") == 0)
-	{
 		value = chdir(getenv("OLDPWD"));
-	}
 	else
 		value = chdir(cmd[1]);
 
@@ -66,5 +30,6 @@ int _cd(char **cmd, __attribute__((unused))int er)
 		setenv("OLDPWD", getenv("PWD"), 1);
 		setenv("PWD", cwd, 1);
 	}
+
 	return (0);
 }
