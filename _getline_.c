d8826e99afcd2f9b17b3de8bb17de2f512ec6f18
mahmoudbalbaa <ma7moud.balbaa@gmail.com>
2023-10-15 14:57:46 -0700

    get line and hash func

--- _getline_.c
+++ _getline_.c
@@ -1,15 +1,15 @@
-
 #include "main.h"
 
 /**
 * _getline_ - Read The Input By User From Stdin
 * Return: Input
 */
+
 char *_getline_()
 {
-int i, buffsize = BUFSIZE, rd;
-char c = 0;
-char *buff = malloc(buffsize);
+	int i, buffsize = BUFSIZE, rd;
+	char c = 0;
+	char *buff = malloc(buffsize);
 
 	if (buff == NULL)
 	{
@@ -20,47 +20,30 @@ char *buff = malloc(buffsize);
 	for (i = 0; c != EOF && c != '\n'; i++)
 	{
 		fflush(stdin);
+
 		rd = read(STDIN_FILENO, &c, 1);
 		if (rd == 0)
 		{
 			free(buff);
 			exit(EXIT_SUCCESS);
 		}
+
 		buff[i] = c;
 		if (buff[0] == '\n')
 		{
 			free(buff);
 			return ("\0");
 		}
+
 		if (i >= buffsize)
 		{
 			buff = _realloc_(buff, buffsize, buffsize + 1);
 			if (buff == NULL)
-			{
 				return (NULL);
-			}
 		}
 	}
 	buff[i] = '\0';
 	_hash_handl(buff);
+
 	return (buff);
-}
-
-/**
- * _hash_handl - remove everything after #
- * @buff: input;
- * Return:void
- */
-void _hash_handl(char *buff)
-{
-	int i;
-
-		for (i = 0; buff[i] != '\0'; i++)
-		{
-			if (buff[i] == '#')
-			{
-			buff[i] = '\0';
-			break;
-			}
-	}
 }
--- _history.c
+++ _history.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * _history - Fill File By User Input
  * @input: User Input
  * Return: -1 Fail 0 Succes
  */
+
 int _history(char *input)
 {
 	char *filename = ".simple_main._history";
@@ -12,30 +14,20 @@ int _history(char *input)
 
 	if (!filename)
 		return (-1);
+
 	fd = open(filename, O_CREAT | O_RDWR | O_APPEND, 00600);
 	if (fd < 0)
 		return (-1);
+
 	if (input)
 	{
 		while (input[len])
 			len++;
 		w = write(fd, input, len);
+
 		if (w < 0)
 			return (-1);
 	}
-	return (1);
-}
-/**
- * _free_env_ - Free Enviroment Variable Array
- * @env:  Environment variables.
- * Return: Void
- */
-void _free_env_(char **env)
-{
-	int i;
 
-	for (i = 0; env[i]; i++)
-	{
-		free(env[i]);
-	}
+	return (1);
 }
